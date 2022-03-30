91d541b58b61766bda31ec5eca129ed8020dd443
Adityasinghmr <102655910+Adityasinghmr@users.noreply.github.com>
2022-03-30 13:56:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCxSfTZP0pEa9Jb0yUc9gEkqeC0hUdX37U",
+    authDomain: "recipe-app-8e851.firebaseapp.com",
+    databaseURL: "https://recipe-app-8e851-default-rtdb.firebaseio.com",
+    projectId: "recipe-app-8e851",
+    storageBucket: "recipe-app-8e851.appspot.com",
+    messagingSenderId: "295683438157",
+    appId: "1:295683438157:web:5818a74c26b15f429a4e04"
+  };
